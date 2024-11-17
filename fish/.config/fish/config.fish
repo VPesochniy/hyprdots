c02b1ec163b99f55ad0a08e3e407b99f1e5301b7
Vova <vpesochniy@yandex.com>
2024-11-17 16:18:51 +0300

    chore(config.fish): update environment variable from GITHUB_TOKEN to OCO_API_KEY for better clarity and usage

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,4 +10,6 @@ alias ls="eza"
 alias ll="eza -l"
 alias la="eza -la"
 
-set -Ux GITHUB_TOKEN ${{ secrets.FISH_GITHUB_TOKEN }}
+# set -Ux GITHUB_TOKEN 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
+set -Ux OCO_API_KEY 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
+# set -Ux GITHUB_TOKEN ${{ secrets.FISH_GITHUB_TOKEN }}
