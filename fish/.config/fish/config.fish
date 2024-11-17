fef542549fcd918ec6141ecabb3577da129ed34a
Vova <vpesochniy@yandex.com>
2024-11-17 16:28:16 +0300

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,5 +11,5 @@ alias ll="eza -l"
 alias la="eza -la"
 
 # set -Ux GITHUB_TOKEN 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
-set -Ux OCO_API_KEY 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
+set -Ux OCO_API_KEY ${{ secrets.OCO_API_KEY }}
 # set -Ux GITHUB_TOKEN ${{ secrets.FISH_GITHUB_TOKEN }}
