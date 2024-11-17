46ce175488e7e5ed3a590ca322e923122ebf7748
Vova <vpesochniy@yandex.com>
2024-11-17 16:36:55 +0300

    chore(fish): clean up config.fish by removing commented-out lines for clarity chore(opencommit): update OCO_API_KEY assignment to prevent accidental exposure of sensitive information

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,7 +9,3 @@ bind \cf "zi; commandline -f repaint"
 alias ls="eza"
 alias ll="eza -l"
 alias la="eza -la"
-
-# set -Ux GITHUB_TOKEN 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
-set -Ux OCO_API_KEY ${{ secrets.OCO_API_KEY }}
-# set -Ux GITHUB_TOKEN ${{ secrets.FISH_GITHUB_TOKEN }}
